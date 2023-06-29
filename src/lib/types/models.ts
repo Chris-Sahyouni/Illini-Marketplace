cfdcb04ab3e372b9961b2d7c2d5163f4c1b86e3e
Chris Sahyouni <102492467+Chris-Sahyouni@users.noreply.github.com>
2023-06-29 12:06:01 -0500

    added questions for every type

--- src/lib/types/models.ts
+++ src/lib/types/models.ts
@@ -15,36 +15,69 @@ export class ItemData {
     type: ItemType = ItemType.UnResolved;
     // image
 
-    public constructor(sublease?: Sublease, textbook?: Textbook, transit?: TransitTicket, sport?: SportsTicket, parking?: Parking, misc?: Misc) {
+    public constructor(sublease?: Sublease, textbook?: Textbook, transit?: TransitTicket, ticket?: SportsTicket, parking?: Parking, misc?: Misc) {
         if (sublease) {
-
+            this.constructFromSublease(sublease);
         }
         if (textbook) {
             this.constructFromTextbook(textbook);
-            this.assignVisibleValues(Object.keys(textbook), Object.values(textbook));
         }
-        if (sport) {
-
+        if (ticket) {
+            this.constructFromTicket(ticket);
         }
         if (parking) {
-
+            this.constructFromParking(parking);
+        }
+        if (transit) {
+            this.constructFromTransit(transit);
         }
         if (misc) {
-
+            this.constructFromMisc(misc);
         }
+    }
+
+    private constructGeneric(itemType: ItemType) {
+        this.type = itemType;
+        this.visibleKeys = typeKeyMap.get(itemType);
+        this.sellQuestions = typeQuestionMap.get(itemType);
     }
 
     private constructFromTextbook(prismaItem: Textbook) {
         this.id = prismaItem.id;
-        this.type = ItemType.Textbook;
-        this.visibleKeys = typeKeyMap.get(ItemType.Textbook);
-        this.sellQuestions = typeQuestionMap.get(ItemType.Textbook);
-
+        this.constructGeneric(ItemType.Textbook);
         this.assignVisibleValues(Object.keys(prismaItem), Object.values(prismaItem));
     }
 
     private constructFromSublease(prismaItem: Sublease) {
+        this.id = prismaItem.id;
+        this.constructGeneric(ItemType.Sublease);
+        this.assignVisibleValues(Object.keys(prismaItem), Object.values(prismaItem));
+    }
 
+    private constructFromTransit(prismaItem: TransitTicket) {
+        this.id = prismaItem.id;
+        this.constructGeneric(ItemType.Transit);
+        this.assignVisibleValues(Object.keys(prismaItem), Object.values(prismaItem));
+    }
+
+
+    // this will have to change to just Ticket
+    private constructFromTicket(prismaItem: SportsTicket) {
+        this.id = prismaItem.id;
+        this.constructGeneric(ItemType.Ticket);
+        this.assignVisibleValues(Object.keys(prismaItem), Object.values(prismaItem));
+    }
+
+    private constructFromParking(prismaItem: Parking) {
+        this.id = prismaItem.id;
+        this.constructGeneric(ItemType.Parking);
+        this.assignVisibleValues(Object.keys(prismaItem), Object.values(prismaItem));
+    }
+
+    private constructFromMisc(prismaItem: Misc) {
+        this.id = prismaItem.id;
+        this.constructGeneric(ItemType.Misc);
+        this.assignVisibleValues(Object.keys(prismaItem), Object.values(prismaItem));
     }
 
     public assignVisibleValues(allKeys: string[], allValues: any[]): void {
@@ -91,22 +124,23 @@ export enum ItemType {
     UnResolved
 }
 
+const contactPair: [string, string] = ['How would you like to be contacted?', 'eg. 123-456-7890, or snap: john_doe123, etc'];
 
-
-export const subleaseKeys:  string[] = ["price", "address", "contact", "company", "numBedrooms", "numBathrooms", "startDate", "endDate", "notes"];
+// is company really necessary?
+export const subleaseKeys:  string[] = ["address", "price", "contact", "company", "numBedrooms", "numBathrooms", "startDate", "endDate", "notes"];
 export const textbookKeys:  string[] = ['course', 'price', 'contact'];
-export const transitKeys:  string[] = [];
-export const ticketKeys:  string[] = [];
-export const parkingKeys:  string[] = [];
-export const miscKeys:  string[] = [];
+export const transitKeys:  string[] = ['mode', 'price', 'date'];
+export const ticketKeys:  string[] = ['event', 'price', 'date', 'seat'];
+export const parkingKeys:  string[] = ['location', 'price', 'startDate', 'endDate'];
+export const miscKeys:  string[] = ['name', 'price', 'notes'];
 
 // these strings are question and placeholder respectively
-export const subleaseQuestions:  [string, string][] = [];
-export const textbookQuestions:  [string, string][] = [['Course Code', 'eg. STAT 100'], ['Price', ''], ['How would you like to be contacted?', 'eg. 123-456-7890, or snap: john_doe123, etc']];
-export const transitQuestions:  [string, string][] = [];
-export const ticketQuestions:  [string, string][] = [];
-export const parkingQuestions:  [string, string][] = [];
-export const miscQuestions:  [string, string][] = [];
+export const subleaseQuestions:  [string, string][] = [['Address/Location', 'eg. 123 E Green, or HERE'], ['Rent (per person/month)', ''], contactPair, ['Company', 'eg. Smile'], ["How many bedrooms", ""], ['How many bathrooms', ''], ['Start Date', ''], ['End Date', ''], ['Any other information you want to add', '']];
+export const textbookQuestions:  [string, string][] = [['Course Code', 'eg. STAT 100'], ['Price', ''], contactPair];
+export const transitQuestions:  [string, string][] = [['What mode', 'eg. Amtrack, Peoria Charter'], ['Price', ''], ['Date', '']];
+export const ticketQuestions:  [string, string][] = [['What event', ''], ['Price', ''], ['Date', ''], ['Seat (if applicable)', '']];
+export const parkingQuestions:  [string, string][] = [['Location', ''], ['Price', ''], ['Start Date', ''], ['End Date', '']];
+export const miscQuestions:  [string, string][] = [['What are you selling', ''], ['Price', ''], ['Any other information you want to add', '']];
 
 
 export const typeKeyMap = new Map<ItemType,  string[]>([
