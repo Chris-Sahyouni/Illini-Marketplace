3eef06775325ead22523b90c490e5e55c3a3a3f0
Chris Sahyouni <102492467+Chris-Sahyouni@users.noreply.github.com>
2023-08-14 13:41:19 -0500

    added success display for sell form submit

--- src/app/sell/form/page.tsx
+++ src/app/sell/form/page.tsx
@@ -10,6 +10,7 @@ import { ItemType, typeKeyMap, typeQuestionMap } from "@/src/lib/maps";
 import { ItemData } from "@/src/lib/types/models";
 import SubleaseCard from "@/src/components/SubleaseCard";
 import {v4 as uuidv4} from "uuid";
+import { STATIC_STATUS_PAGES } from "next/dist/shared/lib/constants";
 
 
     export default function Page() {
@@ -74,12 +75,24 @@ import {v4 as uuidv4} from "uuid";
 
         useEffect(() => {
             assignType();
-            console.log('items Id is: ' + itemId);
         }, [])
+
+        if (status == Status.Success) {
+            return (
+                <div className="flex justify-center my-4">
+                    <div className="flex-col bg-white w-2/5 py-4 px-3 items-center text-center rounded">
+                        <h1 className="font-bold text-lg">Success</h1>
+                        <p className="mt-4 ">You can always edit your listing later</p>
+                    </div>
+                </div>
+            );
+        }
 
         if (data.type === ItemType.UnResolved) {
             assignType()
-            return (<></>);
+            return (
+                <></>
+            );
         }
 
         // there should be lots of checks here before the request even gets sent
