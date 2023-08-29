2a539dd3588c11b62a45ac01c85caaeaff5ba121
Chris Sahyouni <102492467+Chris-Sahyouni@users.noreply.github.com>
2023-08-28 19:50:29 -0500

    next secure headers

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
 
-module.exports = nextConfig
+const { createSecureHeaders } = require('next-secure-headers');
+
+ 
+
+module.exports ={
+    async headers() {
+        return [{ source: "/(.*)", headers: createSecureHeaders() }]
+    }
+};
